1885ca481b54b2d3d56c2ed0a54b9710e5ed2ee2
wtongxuan08@gmail.com <wtongxuan08@gmail.com>
2020-08-10 02:14:08 +0800

    check out

---
+++ cmake-build-debug/CMakeFiles/NCursesConnectN.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/tongxuanwang/Desktop/ECS/ecs 36b code/NcursesConnectN/CursorTrakeWindow.c" "/Users/tongxuanwang/Desktop/ECS/ecs 36b code/NcursesConnectN/cmake-build-debug/CMakeFiles/NCursesConnectN.dir/CursorTrakeWindow.c.o"
+  "/Users/tongxuanwang/Desktop/ECS/ecs 36b code/NcursesConnectN/board.c" "/Users/tongxuanwang/Desktop/ECS/ecs 36b code/NcursesConnectN/cmake-build-debug/CMakeFiles/NCursesConnectN.dir/board.c.o"
+  "/Users/tongxuanwang/Desktop/ECS/ecs 36b code/NcursesConnectN/main.c" "/Users/tongxuanwang/Desktop/ECS/ecs 36b code/NcursesConnectN/cmake-build-debug/CMakeFiles/NCursesConnectN.dir/main.c.o"
+  "/Users/tongxuanwang/Desktop/ECS/ecs 36b code/NcursesConnectN/move.c" "/Users/tongxuanwang/Desktop/ECS/ecs 36b code/NcursesConnectN/cmake-build-debug/CMakeFiles/NCursesConnectN.dir/move.c.o"
+  "/Users/tongxuanwang/Desktop/ECS/ecs 36b code/NcursesConnectN/win.c" "/Users/tongxuanwang/Desktop/ECS/ecs 36b code/NcursesConnectN/cmake-build-debug/CMakeFiles/NCursesConnectN.dir/win.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/NCursesConnectN.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NCursesConnectN.dir/CursorTrakeWindow.c.o"
+  "CMakeFiles/NCursesConnectN.dir/board.c.o"
+  "CMakeFiles/NCursesConnectN.dir/main.c.o"
+  "CMakeFiles/NCursesConnectN.dir/move.c.o"
+  "CMakeFiles/NCursesConnectN.dir/win.c.o"
+  "NCursesConnectN.out"
+  "NCursesConnectN.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NCursesConnectN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
